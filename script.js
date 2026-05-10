cebc969b4b60e89e8f96878f656f35fa56dc6645
aelias-kan <150896358+aelias-kan@users.noreply.github.com>
2026-05-10 16:12:46 +0400

    write js code for game rock paper scissor
    wite a complete code for js game rock paper scissor to play game inside console

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
-console.log("Welcome to Rock Paper Scissors ");
-
-do{
+console.log("Welcome to Rock Paper Scissors Game");
+console.log('#Instructions to start playing type play("rock OR paper OR scissor"); ');
+const Game = ["rock","paper","scissor"];
+let compWinCounter =0;
+let userWinCounter =0;
+let draw =0;
+function play(choice)
+{
     
-}while(choice != "stop");
+    let user_choice = choice.toLowerCase();
+    let compIndex= generateCompChoice();
+    let comp_choice = Game[compIndex];
+    if (user_choice == "rock" || user_choice == "paper" || user_choice =="scissor")
+    {
+        playGame(user_choice,comp_choice);
+    }    
+    else
+    {
+        console.log("Invalid Entry! please enter either Rock/rock Paper/paper or Scissor/scissor")
+    }
+
+    return 'Round completed';
+}
+
+function playGame(user,comp)
+{
+    if (user == comp)
+    {
+        draw += 1;
+        console.log("Its a draw");
+    }
+    else 
+    {
+        if (user == 'rock' && comp == 'scissor'|| user == 'paper' && comp == 'rock' || user == 'scissor' && comp == 'paper')
+        { 
+            userWinCounter +=1;
+            console.log(`your ${user} beats comp's ${comp}`);
+        }
+        else 
+        {
+            compWinCounter +=1;
+            console.log(`comp's ${comp} beats your ${user}`);
+        }
+    }
+    console.log("Game Updates");
+    console.log(`You won : ${userWinCounter}`);
+    console.log(`Comp won : ${compWinCounter}`);
+    console.log(`Draw : ${draw}`); 
+}
+
+function generateCompChoice() { return Math.floor(Math.random()*Game.length)};
